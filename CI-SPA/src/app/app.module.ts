02e056ab4854a4aa135eb603fb46176f824530d6
Michal Harrison <mlh1477@gmail.com>
2019-08-17 13:16:23 -0400

    Video 9 | Make HTTP request get values from API

--- CI-SPA/src/app/app.module.ts
+++ CI-SPA/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ValueComponent } from './value/value.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CI-SPA/src/app/value/value.component.ts
+++ CI-SPA/src/app/value/value.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-value',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./value.component.scss']
 })
 export class ValueComponent implements OnInit {
+  values: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getValues();
+  }
+
+  getValues() {
+    return this.http.get("http://localhost:5000/api/values").subscribe(response => {
+      console.log(response);
+      this.values = response;
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
