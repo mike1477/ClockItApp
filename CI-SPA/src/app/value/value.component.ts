4074faa8712f1d0069e5c7ffe63d8518ee92f424
Michal Harrison <mlh1477@gmail.com>
2019-08-17 08:13:59 -0400

    Video 8 | Generate and Configure Angular Component

---
+++ CI-SPA/src/app/value/value.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.scss']
+})
+export class ValueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
