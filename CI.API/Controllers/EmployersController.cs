ad3d78561ac0e3b5ba9474437104e96b40b1e0b8
Michal Harrison <mlh1477@gmail.com>
2019-08-31 15:23:21 -0400

    Video 17 | Create register method and use Identity to create new users

---
+++ CI.API/Controllers/EmployersController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CI.API.ViewModels;
+using CI.DAL;
+using CI.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployersController : ControllerBase
+    {
+
+        private readonly UserManager<User> _userManager;
+
+        public EmployersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+
+        }
+
+        // Post api/employers/create
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(CreateEmployerViewModel model)
+        {
+            var employer = new User
+            {
+                UserName = model.Username,
+                Email = model.Email
+            };
+            var result = await _userManager.CreateAsync(employer, model.Password);
+
+            if(!result.Succeeded)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CI.API/ViewModels/CreateEmployerViewModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CI.API.ViewModels
+{
+    public class CreateEmployerViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
