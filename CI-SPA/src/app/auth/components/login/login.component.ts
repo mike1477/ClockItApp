85f47fad2d42cdd1c870920f3ca22f91c7a42383
Michal Harrison <mlh1477@gmail.com>
2019-10-01 09:38:46 -0400

    Video 32 | Inject service into component

--- CI-SPA/src/app/auth/components/login/login.component.ts
+++ CI-SPA/src/app/auth/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +9,19 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   onSubmit(f: NgForm) {
-    console.log(f.value);  // { first: '', last: '' }
-    console.log(f.valid);  // false
+
+    const loginObserver = {
+      next: x => console.log('User logged in'),
+      error: err => console.log(err)
+    };
+    this.authService.login(f.value).subscribe(loginObserver);
+
   }
 
 }
