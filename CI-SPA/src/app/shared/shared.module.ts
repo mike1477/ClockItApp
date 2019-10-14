f1f1fd6f37961e506eab64238cd8bcb1fa01c3f9
Michal Harrison <mlh1477@gmail.com>
2019-10-14 11:23:22 -0400

    Video 39 | Use Angular ngx-bootstrap

--- CI-SPA/src/app/shared/shared.module.ts
+++ CI-SPA/src/app/shared/shared.module.ts
@@ -9,6 +9,7 @@ import { NgProgressModule } from '@ngx-progressbar/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AlertModule } from 'ngx-alerts';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 
 @NgModule({
@@ -19,6 +20,7 @@ import { AlertModule } from 'ngx-alerts';
     NgProgressModule,
     BrowserAnimationsModule,
     BrowserModule,
+    BsDropdownModule.forRoot(),
 
     // Specify your library as an import (set timeout to -1 for unlimited timeout, the message can only be closed by the user clicking on it)
     AlertModule.forRoot({ maxMessages: 5, timeout: 5000, position: 'right' })
